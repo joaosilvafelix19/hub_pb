c5ea71872afc49e760149a26fea08a3c917abe2f
João Felix <joaosilvafelix19@gmail.com>
2023-08-21 10:10:51 -0300

    Update 5_ Lucro_Real.py

--- pages/5_ Lucro_Real.py
+++ pages/5_ Lucro_Real.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 st.title("CNPJs com regime tributário do tipo 'Lucro Real'")
 
-st.markdown("Abaixo é mostrado quantos CNPJ's o regime tributário é do tipo 'Lucro real' no estado da Paraíba entre os anos de 2014 e 2021. Os dados estão disponíveis no portal de dados abertos do governo federal. As informações sobre CNPJs estão [aqui](https://dados.gov.br/dados/conjuntos-dados/cadastro-nacional-da-pessoa-juridica---cnpj), enquanto que as informações sobre CNPJs inseridos no regime tributário do tipo lucro real estão disponíveis [aqui](http://200.152.38.155/CNPJ/regime_tributario/)".)
+st.markdown("Abaixo é mostrado quantos CNPJ's o regime tributário é do tipo 'Lucro real' no estado da Paraíba entre os anos de 2014 e 2021. Os dados estão disponíveis no portal de dados abertos do governo federal. As informações sobre CNPJs estão [aqui](https://dados.gov.br/dados/conjuntos-dados/cadastro-nacional-da-pessoa-juridica---cnpj), enquanto que as informações sobre CNPJs inseridos no regime tributário do tipo lucro real estão disponíveis [aqui](http://200.152.38.155/CNPJ/regime_tributario/).")
 
 
 ano = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
