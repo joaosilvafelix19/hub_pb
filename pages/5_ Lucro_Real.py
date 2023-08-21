b68ad4c79aa0095c1fc5c6e8fac106dd56e8bc9b
João Felix <joaosilvafelix19@gmail.com>
2023-08-21 09:04:26 -0300

    Update 5_ Lucro_Real.py

---
+++ pages/5_ Lucro_Real.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import streamlit as st
+import os
+
+import plotly.express as px
+import numpy as np
+import plotly as plt
+
+import plotly.graph_objects as go
+
+from st_aggrid import AgGrid
+from st_aggrid.grid_options_builder import GridOptionsBuilder
